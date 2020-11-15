79416792a0fb6b018238afed447ba99a88da7abd
DZoltan <55488265+DZoltan@users.noreply.github.com>
2020-11-15 21:44:22 +0100

    Initial commit

---
+++ CurrencyChanger/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CurrencyChanger
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Calculating calculate = new Calculating();
+        List<string> CurrencyList = new List<string> { "HUF", "EUR", "GBP", "USD", "HKD", "IDR", "ILS", "DKK", "INR" ,"CHF", "MXN",
+        "CZK" , "SGD", "THB" , "HRK", "MYR", "NOK", "CNY", "BGN", "PHP", "SEK", "PLN", "ZAR", "CAD" , "ISK", "BRL", "RON", "NZD",
+        "TRY", "JPY", "RUB", "KRW", "AUD"};
+        public MainPage()
+        {
+            //Json_save save = new Json_save();
+            this.InitializeComponent();
+            calculate_btn.IsEnabled = false;
+
+        }
+
+        private void swap_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                to_cash.Text = "";
+                from_cash.Text = "";
+                string temp = to_currency.SelectedItem.ToString();
+                to_currency.SelectedItem = from_currency.SelectedItem;
+                from_currency.SelectedItem = temp;
+            }
+            catch (NullReferenceException exc)
+            {
+                errormsg_txt.Text = "Nem adtál meg valutát!";
+            }
+        }
+
+        private void calculate_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                to_cash.Text = calculate.calculate_currency(from_currency.SelectedItem.ToString(), double.Parse(from_cash.Text), to_currency.SelectedItem.ToString()).ToString();
+                errormsg_txt.Text = "";
+            }
+            catch (NullReferenceException exc)
+            {
+                errormsg_txt.Text = "Nem adtál meg valutát!" ;
+            }
+        }
+
+        private void from_cash_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double checked_cash;
+            if(from_cash.Text == "")
+            {
+                calculate_btn.IsEnabled = false;
+                errormsg_txt.Text = "";
+            }
+            else if(!Double.TryParse(from_cash.Text, out checked_cash))
+            {
+                calculate_btn.IsEnabled = false;
+                errormsg_txt.Text = "A megadott érték nem szám!";
+
+            }
+            else if (from_cash.Text.Contains(","))
+            {
+                calculate_btn.IsEnabled = false;
+                errormsg_txt.Text = "Használj tizedespontot! (pl: 12,5 helyett 12.5)";
+            }
+            else
+            {
+                calculate_btn.IsEnabled = true;
+                errormsg_txt.Text = "";
+            }
+        }
+
+        private void from_currency_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CurrencyChanger/Model/Calculating.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyChanger
+{
+    class Calculating
+    {
+        Deseralizer rates = new Deseralizer();
+
+
+        public double calculate_currency(string from, double cash, string to)
+        {
+            double calculated_cash = 0;
+
+            double from_currency = currency_rates(from);
+            double to_currency = currency_rates(to);
+            double currency_rate = to_currency / from_currency;
+
+            calculated_cash = cash * currency_rate;
+
+
+            return Math.Round(calculated_cash,2);
+
+        } 
+
+        private double currency_rates(string currency)
+        {
+            double _cash = 0;
+
+            switch (currency)
+            {
+                case "EUR": _cash = 1; break;
+                case "GBP": _cash = rates._rated.rates.GBP; break;
+                case "HKD": _cash = rates._rated.rates.HKD; break;
+                case "IDR": _cash = rates._rated.rates.IDR; break;
+                case "ILS": _cash = rates._rated.rates.ILS;  break;
+                case "DKK": _cash = rates._rated.rates.DKK; break;
+                case "INR": _cash = rates._rated.rates.INR; break;
+                case "CHF": _cash = rates._rated.rates.CHF; break;
+                case "MXN": _cash = rates._rated.rates.MXN; break;
+                case "CZK": _cash = rates._rated.rates.CZK; break;
+                case "SGD": _cash = rates._rated.rates.SGD; break;
+                case "THB": _cash = rates._rated.rates.THB; break;
+                case "HRK": _cash = rates._rated.rates.HRK; break;
+                case "MYR": _cash = rates._rated.rates.MYR; break;
+                case "NOK": _cash = rates._rated.rates.NOK; break;
+                case "CNY": _cash = rates._rated.rates.CNY; break;
+                case "BGN": _cash = rates._rated.rates.BGN; break;
+                case "PHP": _cash = rates._rated.rates.PHP; break;
+                case "SEK": _cash = rates._rated.rates.SEK; break;
+                case "PLN": _cash = rates._rated.rates.PLN; break;
+                case "ZAR": _cash = rates._rated.rates.ZAR; break;
+                case "CAD": _cash = rates._rated.rates.CAD; break;
+                case "ISK": _cash = rates._rated.rates.ISK; break;
+                case "BRL": _cash = rates._rated.rates.BRL; break;
+                case "RON": _cash = rates._rated.rates.RON; break;
+                case "NZD": _cash = rates._rated.rates.NZD; break;
+                case "TRY": _cash = rates._rated.rates.TRY; break;
+                case "JPY": _cash = rates._rated.rates.JPY; break;
+                case "RUB": _cash = rates._rated.rates.RUB; break;
+                case "KRW": _cash = rates._rated.rates.KRW; break;
+                case "USD": _cash = rates._rated.rates.USD; break;
+                case "HUF": _cash = rates._rated.rates.HUF; break;
+                case "AUD": _cash = rates._rated.rates.AUD; break;
+                default: _cash = 404; break;
+            }
+
+            return _cash;
+        }
+    }
+}
